3b1a5135459ade1144f6b5d526c1f07a37b04a46
fredeskrubba <frederikskrubbeltrang@outlook.dk>
2022-08-17 09:54:50 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
+import Nav from "./components/Nav";
+import logo from "./images/logo.png";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Nav 
+          logo={logo}
+          logoAlt="Page logo"
+          links={[
+            {
+              name: "Home",
+              route: "/"
+            },
+            {
+              name: "Rumfærgen",
+              route: "/Rumfærgen"
+            },
+            {
+              name: "Ture",
+              route: "/Ture"
+            },
+            {
+              name: "Galleri",
+              route: "/Galleri"
+            },
+            {
+              name: "Sikkerhed",
+              route: "/Sikkerhed"
+            },
+            {
+              name: "Kontakt",
+              route: "/Kontakt"
+            },
+
+          ]}
+        />
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
+    
   );
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,19 @@
-const Nav = () => {
+import { Link } from "react-router-dom";
+
+const Nav = (props) => {
+    
     return ( 
-        <nav>
-            
+        <nav className="container flex my-5 mx-72 flex-row">
+            <section>
+                <img src={props.logo} alt={props.logoAlt} />
+            </section>
+            <section>
+                <div>
+                    {props.links.map(link => {
+                        return <Link to={link.route}> {link.name} </Link>
+                    })}
+                </div>
+            </section> 
         </nav>
      );
 }
